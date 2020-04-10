f7a436fbea0bbcda6745a25f18d32013331e755b
rajpalsinghd <rajpalsinghd@outlook.com>
2020-04-10 14:46:17 +0530

    repo service web created

---
+++ ppmtoolapi/src/main/java/com/example/demo/domain/Project.java
@@ -0,0 +1,86 @@
+package com.example.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import java.util.*;
+@Entity
+public class Project {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String projectName;
+	private String projectIdentifier;
+	public Long getId() {
+		return id;
+	}
+	public Project() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public Project(Long id, String projectName, String projectIdentifier, String description, Date start_date,
+			Date end_date, Date created_at) {
+		super();
+		this.id = id;
+		this.projectName = projectName;
+		this.projectIdentifier = projectIdentifier;
+		this.description = description;
+		this.start_date = start_date;
+		this.end_date = end_date;
+		this.created_at = created_at;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getProjectIdentifier() {
+		return projectIdentifier;
+	}
+	public void setProjectIdentifier(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	private String description;
+	private Date start_date;
+	private Date end_date;
+	private Date created_at;
+	private Date updated_at;
+	@PrePersist
+	public void onCreate()
+	{
+		
+		this.created_at=new Date();
+	}
+	@PreUpdate
+	public void onUpdate()
+	{
+		this.updated_at=new Date();
+	}
+
+}
---
+++ ppmtoolapi/src/main/java/com/example/demo/web/ProjectController.java
@@ -0,0 +1,26 @@
+package com.example.demo.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Project;
+import com.example.demo.service.ProjectService;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+	@Autowired
+	private ProjectService projectService;
+	@PostMapping("")
+	public ResponseEntity<Project> createNewProject(@RequestBody Project project)
+	{
+		Project proj=projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(proj,HttpStatus.CREATED);
+	}
+}
